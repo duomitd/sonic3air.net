a14e76cd6cdcc138e888bdc46405e09690a44379
leo <leo@gmail.com>
2024-12-08 02:09:25 +0800

    fix bug

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -4,6 +4,8 @@ import matter from "gray-matter";
 
 export function getMdxContent(filename: string) {
   const filePath = path.join(process.cwd(), filename);
+  console.log(filePath);
+
   const fileContent = fs.readFileSync(filePath, "utf8");
   const { data, content } = matter(fileContent);
   return { metadata: data, content };
